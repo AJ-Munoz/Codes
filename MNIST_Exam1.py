63fa9fb56a7d4fb9aeea031c6c66c45b65b081eb
AJ-Munoz <aj.munozvazquez@gmail.com>
2026-02-15 00:38:38 -0700

    Commit now

--- MNIST_Exam1.py
+++ MNIST_Exam1.py
@@ -1,50 +1,54 @@
+
 import tensorflow as tf
 from tensorflow.keras import layers, models
 import matplotlib.pyplot as plt
 import numpy as np
 
+# 1) Load data
 (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
-train_images = train_images.astype('float32') / 255
-test_images = test_images.astype('float32') / 255
 
-print('Number of images in the training dataset:',    train_images.shape[0])
-print('Number of images in the testing dataset:',     test_images.shape[0])
-print(f"Shape of the images in the training dataset: {train_images[0].shape}")
+# 2) Scale to [0,1]
+train_images = train_images.astype('float32') / 255.0
+test_images  = test_images.astype('float32') / 255.0
 
-fig, axes = plt.subplots(1, 10, figsize=(10, 10))
-for i in range(10):
-    axes[i].imshow(train_images[i].reshape(28, 28), cmap='gray')
-    axes[i].set_title(train_labels[i])
-    axes[i].axis('off')
-plt.show()
-
+# 3) Build a fully-connected (dense) network
+#    NOTE: input_shape is (28, 28) because images are 2D without an explicit channel dim.
 model = models.Sequential([
-    layers.Flatten(input_shape=(28, 28, 1)),
+    layers.Flatten(input_shape=(28, 28)),
     layers.Dense(128, activation='relu'),
-    layers.Dense(64, activation='relu'),
+    #layers.Dense(64, activation='relu'),
     layers.Dense(10, activation='softmax')
 ])
 
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
-
-history = model.fit(
-    train_images, 
-    train_labels, 
-    epochs=10
+# 4) Compile with sparse labels (integers 0..9)
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
 )
 
-def view_classify(image, probabilities):
-    fig, (ax1, ax2) = plt.subplots(figsize=(6,9), ncols=2)
-    ax1.imshow(image)
-    ax1.axis('off')
-    ax2.barh(np.arange(10), probabilities)
-    ax2.set_aspect(0.1)
-    ax2.set_yticks(np.arange(10))
-    ax2.set_yticklabels(np.arange(10))
-    ax2.set_title('Class Probability')
-    ax2.set_xlim(0, 1.1)
-    plt.tight_layout()
+# 5) Train 
+model.fit(
+    train_images,
+    train_labels,
+    epochs=5,
+    verbose=1
+)
 
-image, label = test_images[0], test_labels[0]
+# 6) Quick dataset info
+print('Number of images in the training dataset:', train_images.shape[0])
+print('Number of images in the testing dataset:',  test_images.shape[0])
+print(f"Shape of one training image: {train_images[0].shape}")
+
+# 7) Visualize a few training examples
+fig, axes = plt.subplots(1, 10, figsize=(12, 2))
+for i in range(10):
+    axes[i].imshow(train_images[i], cmap='gray')
+    axes[i].set_title(str(train_labels[i]))
+    axes[i].axis('off')
+plt.tight_layout()
+plt.show()
+
+# 8) Evaluate on test set
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=0)
+print(f'\nTest accuracy: {test_acc:.4f}')
